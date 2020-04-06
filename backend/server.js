a1f891ffc1a7ccc67c0fc41d4eeb76af29a64762
Emanuel Ben Hamo <embenh@gmail.com>
2020-04-06 22:23:59 +0300

    replaced to template string

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8081','http://127.0.0.1:8081', 'http://localhost:8080', 'http://127.0.0.1:3030', 'http://localhost:3030'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8081', 'http://127.0.0.1:8081', 'http://localhost:8080', 'http://127.0.0.1:3030', 'http://localhost:3030'],
         credentials: true
     };
     app.use(cors(corsOptions));
@@ -40,5 +40,5 @@ const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030;
 
 http.listen(port, () => {
-    logger.info('Server is running on port: ' + port)
+    logger.info(`Server is running on port: ${port}`);
 });
